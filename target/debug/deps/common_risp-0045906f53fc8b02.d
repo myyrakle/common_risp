dc162468667693c42b5c8c470438c90da9bc4411
myyrakle <sssang97@naver.com>
2024-01-23 01:05:37 +0900

    init: risp_macro

--- target/debug/deps/common_risp-0045906f53fc8b02.d
+++
@@ -1,5 +0,0 @@
-/home/myyrakle/Codes/Rust/common_risp/target/debug/deps/libcommon_risp-0045906f53fc8b02.rmeta: common_risp/src/lib.rs
-
-/home/myyrakle/Codes/Rust/common_risp/target/debug/deps/common_risp-0045906f53fc8b02.d: common_risp/src/lib.rs
-
-common_risp/src/lib.rs:
