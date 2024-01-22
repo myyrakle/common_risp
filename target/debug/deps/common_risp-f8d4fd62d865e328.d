bc756a2fe147bb0a9ad0d5dabc1ac0199b674a30
myyrakle <sssang97@naver.com>
2024-01-23 01:04:16 +0900

    init workspace

---
+++ target/debug/deps/common_risp-f8d4fd62d865e328.d
@@ -0,0 +1,5 @@
+/home/myyrakle/Codes/Rust/common_risp/target/debug/deps/libcommon_risp-f8d4fd62d865e328.rmeta: common_risp/src/lib.rs
+
+/home/myyrakle/Codes/Rust/common_risp/target/debug/deps/common_risp-f8d4fd62d865e328.d: common_risp/src/lib.rs
+
+common_risp/src/lib.rs:
