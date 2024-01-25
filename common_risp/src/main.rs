17b79fc092e219a473b3dd811ce6e268869ef2b2
myyrakle <sssang97@naver.com>
2024-01-26 03:20:04 +0900

    [#3] 비교연산자 구현

--- common_risp/src/main.rs
+++ common_risp/src/main.rs
@@ -2,5 +2,7 @@ fn main() {
     common_risp::compile!(
         (print 99)
         (print (+ 1 2 (*3 4) 5))
+        (print (>= 10 20 30))
+        (print (/= 10 20 30))
     );
 }
--- risp_macro/src/lib.rs
+++ risp_macro/src/lib.rs
@@ -14,13 +14,21 @@ fn compile_expression(expression: Group) -> String {
         .next()
         .expect("Every root expression must have a function name.")
         .to_string();
-    let function_name = function_name.as_str();
+    let mut function_name = function_name;
+
+    if function_name == ">" || function_name == "<" || function_name == "/" {
+        let next_token = group_iter.peek().unwrap().to_string();
+        if next_token == "=" {
+            group_iter.next();
+            function_name += next_token.as_str();
+        }
+    }
 
     let tokens = group_iter.collect::<Vec<_>>();
 
     let mut rust_code = "".to_string();
 
-    match function_name {
+    match function_name.as_str() {
         "+" | "-" | "*" | "/" => {
             let mut operand = vec![];
 
@@ -37,7 +45,49 @@ fn compile_expression(expression: Group) -> String {
                 }
             }
 
-            rust_code.push_str(&format!("({})", operand.join(function_name)));
+            rust_code.push_str(&format!("({})", operand.join(function_name.as_str())));
+        }
+        "<" | ">" | "<=" | ">=" | "=" | "/=" => {
+            let mut operand = vec![];
+
+            let operator = match function_name.as_str() {
+                "<" => "<",
+                ">" => ">",
+                "<=" => "<=",
+                ">=" => ">=",
+                "=" => "==",
+                "/=" => "!=",
+                _ => unreachable!(),
+            };
+
+            for token in tokens {
+                match token {
+                    TokenTree::Group(group)
+                        if group.delimiter() == proc_macro::Delimiter::Parenthesis =>
+                    {
+                        operand.push(compile_expression(group));
+                    }
+                    _ => {
+                        operand.push(token.to_string());
+                    }
+                }
+            }
+
+            if operand.len() < 2 {
+                panic!("{} must have at least two operands.", function_name);
+            }
+
+            let mut binary_expressions = vec![];
+            for i in 0..operand.len() - 1 {
+                binary_expressions.push(format!(
+                    "({} {} {})",
+                    operand[i],
+                    operator,
+                    operand[i + 1]
+                ));
+            }
+
+            rust_code.push_str(&format!("({})", binary_expressions.join("&&")));
         }
         "defparameter" => {
             panic!("defparameter is not supported.")
@@ -46,7 +96,7 @@ fn compile_expression(expression: Group) -> String {
             panic!("defun is not supported.")
         }
         _ => {
-            let function_name = replace_spetial_function(function_name);
+            let function_name = replace_spetial_function(function_name.as_str());
 
             let mut parameters = vec![];
 
